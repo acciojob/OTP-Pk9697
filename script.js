95a3cf5ea8c3e7f77d18d782314b2b680b6dd54b
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-05-27 18:37:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 //your JS code here. If required.
+
+const firstCode=document.getElementById(`code-1`).focus()
 for(let currentCodeCount=1;currentCodeCount<=6;++currentCodeCount){
 	const currentCode=document.getElementById(`code-${currentCodeCount}`)
-	currentCode.addEventListener('input',()=>{
-		if(currentCodeCount==6) return;
-		const nextCode=document.getElementById(`code-${currentCodeCount+1}`)
-		nextCode.focus()
+	currentCode.addEventListener('input',(event)=>{  
+		const {value}=event.target 
+		if(value>=0 && value<=9){  
+			if(currentCodeCount==6) return;
+			const nextCode=document.getElementById(`code-${currentCodeCount+1}`)
+			nextCode.focus()
+		}
 	})
 	currentCode.addEventListener('keydown',(event)=>{ 
-		const key = event.key; 
+		const key = event.key;
 		if (key !== "Backspace") {
 			return;
 		}
 		
-		if(currentCodeCount==6) {
+		if(currentCodeCount==6) { 
 			if(currentCode.value!==""){
 				currentCode.value="" 
 				return;
