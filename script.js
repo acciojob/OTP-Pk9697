15bac3e11031b8f169ed8bc8cf589626f5de8219
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-05-27 18:28:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+for(let currentCodeCount=1;currentCodeCount<=6;++currentCodeCount){
+	const currentCode=document.getElementById(`code-${currentCodeCount}`)
+	currentCode.addEventListener('input',()=>{
+		if(currentCodeCount==6) return;
+		const nextCode=document.getElementById(`code-${currentCodeCount+1}`)
+		nextCode.focus()
+	})
+	currentCode.addEventListener('keydown',(event)=>{ 
+		const key = event.key; 
+		if (key !== "Backspace") {
+			return;
+		}
+		
+		if(currentCodeCount==6) {
+			if(currentCode.value!==""){
+				currentCode.value="" 
+				return;
+			}
+		}
+		if(currentCodeCount==1) return;
+		const prevCode=document.getElementById(`code-${currentCodeCount-1}`)
+		prevCode.value=""
+		prevCode.focus()
+	})
+}
+
